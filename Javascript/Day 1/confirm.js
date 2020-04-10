0c50027472e01a1372dd4a46d48a067e1b4bbf04
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-04-10 18:52:15 +0200

    Create confirm.js

---
+++ Javascript/Day 1/confirm.js
@@ -0,0 +1,11 @@
+function rec(){
+    var mymessage= confirm("");
+    if(mymessage==true)
+    {
+    	document.write("你是女士!");
+    }
+    else
+    {
+        document.write("你是男士!");
+    }
+  } 
