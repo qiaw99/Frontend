7c17e94ce35400b0116b12f70c2f5a6f02f801d3
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-04-12 11:31:34 +0200

    Create display.js

---
+++ Javascript/Day 2/display.js
@@ -0,0 +1,10 @@
+function hidetext()  
+{  
+    var mychar = document.getElementById("con");
+        mychar.style.display="none";
+}  
+function showtext()  
+{  
+    var mychar = document.getElementById("con");
+        mychar.style.display = "block";
+}
