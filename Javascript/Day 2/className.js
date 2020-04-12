cb30884082ada1cdaf263f981ae31ac475a6345f
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-04-12 11:52:32 +0200

    Create className.js

---
+++ Javascript/Day 2/className.js
@@ -0,0 +1,8 @@
+function add(){
+    var p1 = document.getElementById("p1");
+    p1.className="类名为one";
+ }
+ function modify(){
+    var p2 = document.getElementById("p2");
+    p2.className="类名为two";
+ }
